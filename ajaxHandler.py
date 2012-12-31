fae58e0b602f24047b82c666e53f0726db83c267
Mb01 <mbowden01@gmail.com>
2012-12-31 19:40:33 +0900

    Update to Ajax and templates

--- ajaxHandler.py
+++ ajaxHandler.py
@@ -4,19 +4,32 @@ Created on Dec 29, 2012
 @author: mark
 '''
 
+import random
 import webapp2
 from envdef import Handler
 from dbfunc import getQuestions
 
 HTML_TEMPLATE = "mainpage.html"
 
+def questionForm():
+    questions = getQuestions()
+    if not questions:
+        return "Couldn't get a question."
+    q  = random.choice(questions)
+    
+    data_out = q['question']
+    
+        
+
 class AjaxHandler(Handler):
     def get(self):
         #lets just get it to the point where it can return a question
-        questions = getQuestions
-        self.response.out.write(data_out)
-    def post(self):
+        data_out = questionForm()
         self.response.out.write(data_out)
         
+    def post(self):
+        pass
+        #self.response.out.write(data_out)
+
 app = webapp2.WSGIApplication([('/ajax', AjaxHandler)], debug=True)
 
