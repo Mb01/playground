9b1d17840c9afc2889bb66a89d6e2d1ca9754240
Mb01 <mbowden01@gmail.com>
2013-01-02 20:47:41 +0900

    Can display ratings now.

--- mainpage.py
+++ mainpage.py
@@ -12,10 +12,11 @@ HTML_TEMPLATE = "mainpage.html"
 class MainPage(Handler):
     def get(self, arg):
         user = self.testCookie('user')
+        rating = str(self.testCookie('rating'))[len(user):len(user)+7]
         if not user:
             self.redirect("/login")
              
-        self.render(HTML_TEMPLATE, username=user)#arg=arg etc...
+        self.render(HTML_TEMPLATE, username=user, rating=rating)#arg=arg etc...
         
     def post(self, arg):
         self.redirect("/")
--- static/mainpage.js
+++ static/mainpage.js
@@ -1,15 +1,20 @@
 //Created on Dec 2, 2012
 //author: mark
-
 //included in head of mainpage.html
 
-/*How to ajax
-$(document).ready(function(){
-	$.get('/ajax').done(function( data){
-		alert(data);
-	});
-});
-*/
+
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
 
 var attach = function(){
 	$cb = $("#createButton").parent();
