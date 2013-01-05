6acecdb7be6e6228b35c2d77f0c3de5b3d62eb7d
Mb01 <mbowden01@gmail.com>
2013-01-05 19:26:13 +0900

    saving before branch

--- mainpage.py
+++ mainpage.py
@@ -14,6 +14,7 @@ class MainPage(Handler):
         user = self.testCookie('user')
         if not user:
             self.redirect("/login")
+            return
         rating = str(self.testCookie('rating'))[len(user):len(user)+7]
         
              
